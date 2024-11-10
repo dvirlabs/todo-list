8b2b355542c3e38e7342673485226e13769ebaba
dvirlabs <dvirlabs@gmail.com>
2024-11-10 19:16:49 +0200

    style form

--- frontend/src/components/InsertNewTask.jsx
+++ frontend/src/components/InsertNewTask.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import '../style/InsertNewTask.css';
 import { createTask } from "../services/tasksTableService";
+import { TextField, Button, Box } from '@mui/material';
+
 
 
 const InsertNewTask = () => {
@@ -33,13 +35,13 @@ const InsertNewTask = () => {
     };
 
     return (
-        <div className="insert-new-task-container">
-            <form onSubmit={handleSubmit}>
+        <div className="insert-new-task-container" style={{marginTop: '10px', marginBottom: '10px'}}>
+            <Box onSubmit={handleSubmit}>
                 <input type="text" name="task" value={task.task} onChange={handleChange} placeholder="Task" />
                 <input type="text" name="status" value={task.status} onChange={handleChange} placeholder="Status" />
                 <input type="text" name="notes" value={task.notes} onChange={handleChange} placeholder="Notes" />
-                <button type="submit">Add Task</button>
-            </form>
+                <Button type="submit" variant="contained" color="success" style={{ marginLeft: '10px', marginTop: '7px' }} >Add Task</Button>
+            </Box>
         </div>
     );
 };
