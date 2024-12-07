f476f974f11b284a914d801781b8ef096e6dbdc2
dvirlabs <dvirlabs@gmail.com>
2024-12-08 01:13:11 +0200

    App working without events

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,8 +1,14 @@
-CREATE DATABASE todo_list;
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'todo_list') THEN
+        CREATE DATABASE todo_list;
+    END IF;
+END
+$$;
 
-\c todo_list
+\connect todo_list
 
-CREATE TABLE tasks (
+CREATE TABLE IF NOT EXISTS tasks (
     id SERIAL PRIMARY KEY,
     task TEXT NOT NULL,
     status VARCHAR(12) NOT NULL CHECK (status IN ('todo', 'in progress', 'done')),
