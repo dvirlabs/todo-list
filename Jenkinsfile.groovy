1ad8be550c5af732f777874d4fcc3ad9888025a3
dvirlabs <dvirlabs@gmail.com>
2025-01-22 02:07:27 +0200

    add sleep to jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ pipeline {
         stage('Build') {
             steps {
                 echo 'Building..'
-                sleep 5
+                sleep 20
             }
         }
         stage('Test') {
